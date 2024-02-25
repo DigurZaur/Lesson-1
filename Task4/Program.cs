aecaffda0a725ee9d1b8ea0ae5ea25a158a7a847
Zaurbek Digurov <digurzaur@yandex.ru>
2024-02-26 00:57:52 +0300

    finished Lesson 1 Task 4

---
+++ Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int g1 = 2 ;
+int g2 = 9 ;
+int g3 = 9 ;
+int g4 = 5 ;
+int g5 = 6 ;
+
+int max = g1 ;
+
+if (g2 > max)
+{
+    max = g2;
+}
+if (g3 > max)
+{
+    max = g3;
+}
+if (g4 > max)
+{
+    max = g4;
+}
+if (g5 > max)
+{
+    max = g5;
+}
+Console.WriteLine(max);
